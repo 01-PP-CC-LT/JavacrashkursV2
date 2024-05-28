0980135a88e9cbb87df4ae718d7b62665ebdcee2
Lawheee <ltillig@gmail.com>
2024-05-28 12:32:20 +0200

    +forSchleife

--- JavacrashkursV2/src/g_Schleifen/forSchleife.java
+++ JavacrashkursV2/src/g_Schleifen/forSchleife.java
@@ -2,6 +2,10 @@ package g_Schleifen;
 
 public class forSchleife {
     public static void main(String[] args) {
+
+        for (int i = 0; i <= 10; i++) { //Anfangswert = 0; solange i kleiner oder gleich 10 ist (i < 10) addiere 1 in jeder Schleife(i++)
+            System.out.println(i);     //und gib den resultierenden Wert in der Konsole aus 
+        }
         
     }
 
@@ -10,4 +14,8 @@ public class forSchleife {
 /* Schleifen ermöglichen das Widerholte ausführen von Codeblöcken, bis das gewünschte ergebnis erreicht wird
  * Mit schleifen ist es möglich Datenstrukturen wie z.B. Arrays und Listen zu befüllen
  * 
+ * ----------------------------------------------------------
+ * 
+ * Eine for-Schleife wird auch Zählerschleife genannt und wird genutzt wenn wir genau wissen, wie oft eine Schleife ausgeführt werden soll
+ * Die for-Schleife ist KOPFGESTEUERT
  */
