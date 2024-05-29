1156f5c9d42621e52184fd1806f6f014345cd96a
Lawheee <ltillig@gmail.com>
2024-05-29 12:57:24 +0200

    ergänzung mehrdimensionaleArrays

--- JavacrashkursV2/src/b_variablen/c_mehrdimensionaleArrays.java
+++ JavacrashkursV2/src/b_variablen/c_mehrdimensionaleArrays.java
@@ -15,23 +15,33 @@ public class c_mehrdimensionaleArrays {
         firstAndLastName[2][0] = "Super";
         firstAndLastName[2][1] = "Mario";
 
-        for (int i = 0; i < firstAndLastName.length; i++) {
-            for (int j = 0; j < firstAndLastName[i].length; j++) {
-                System.out.println(firstAndLastName[i][j]);
+        firstAndLastName[3][0] = "Fox";
+        firstAndLastName[3][1] = "McCloud";
+
+        firstAndLastName[4][0] = "Dio";
+        firstAndLastName[4][1] = "Brando";
+
+        for (int i = 0; i < firstAndLastName.length; i++) {          //Äußere for-Schleife für den Zeilenindex
+            for (int j = 0; j < firstAndLastName[i].length; j++) {   //Innere for-Schleife für den Spaltentindex
+                System.out.print(firstAndLastName[i][j] + " ");
                 
             }
-            
+            System.out.println();
         }
     }
 
 }
-/*Bei mehrdimensionalen Arrays handelt es sich um Datensammlungen, welche mehrere Dimensionen verwenden kann,
- *wie zum beispiel eine Tabelle (zweidimensional) oder ein würfel (dreidimensional) 
+/*Bei mehrdimensionalen Arrays handelt es sich um Datensammlungen, welche mehrere Dimensionen verwenden können,
+ beispielsweise eine Tabelle (zweidimensional) oder ein Würfel (dreidimensional) 
  Wir sprechen hier einen komplexen index an, 
  also brauchen wir bei einem zweidimensionalen Array zwei koordinaten um das entsprechende Element anwählen zu können
  In einem zweidimensionalen Array (beispielsweise einer Tabelle) benötigen wir den Index der Spalte und den Index der Zeile in der sich das Element befindet,
  welches wir aufrufen möchten
- *
- * bei einem dreidimensionalen Array benötigen wir entsprechend 3 koordinaten 
- * es sind auch weitere Dimensionen als 3 möglich, jedoch wird dies selten verwendet da die visualisierung von mehr als drei Dimensonen sehr abstrakt ist 
+
+ ---------------------------------------------------------------------
+bei einem dreidimensionalen Array benötigen wir entsprechend 3 koordinaten 
+es sind auch weitere Dimensionen als 3 möglich, jedoch wird dies selten verwendet da die visualisierung von mehr als drei Dimensonen sehr abstrakt ist 
+
+ --------------------------------------------------------------------- 
+Die mehrdimensionalen Arrays sind prinzipell genauso aufgebaut wie eindimensionale, wir sprechen lediglich mehrere Koordinaten gleichzeitig an 
  */
