ecdc098c2d578ea99ebc995fbf9ea468f8cf482c
Lawheee <ltillig@gmail.com>
2024-06-03 16:15:13 +0200

    +verschriftlichung OoP

---
+++ JavacrashkursV2/src/i_objektorientierung/a_objektorientierung_oop.java
@@ -0,0 +1,42 @@
+/* Was versteht man unter Objektorientierter Programmierung (OOP)
+
+Unter Oop verstehen wir einen modernen Programmierstil, der sehr nah an das Menschliche denken
+geknüpft ist.
+Alle Dinge um uns herum sind Objekte.
+Die Oop versucht nun ähnliche Objekte mittels Programmierung darzustellen.
+Der Gegensatz zur Oop ist die Prozedurale Programmierung (PP).
+Moderne sprachen wie Java c# oder c++ sind Oop
+---------------------------------------------------------------------------------------------------
+Objekte werden definiert als: Beschreibbare Dinge, die zusammenarbeiten können,
+um zur Problemlösung beizutragen
+Dies bedeutet: 
+Jedes Objekt hat bestimmte, genaue beschreibbare eigentschaften z.B. Ein Stift hat eine bestimmte Farbe, größe, Hersteller usw.
+
+Objekte sind miteinander verknüpfbar um ein Ergebnis zu erzielen z.B. Der Stift kann mit dem Papier verknüpft werden,
+um schrift zu erzeugen
+
+Jedes mal wenn ein Programm geschrieben wird, soll indirekt ein Problem (mit dem fertigen Programm) gelöst werden 
+
+Beispiel anhand eines Videospiels
+
+Spielfigur: 
+    - Hat Lebenspunkte (Eigenschaft)
+    - Hat einen Namen (Eingeschaft)
+    - Kann sich bewegen (Methode)
+    - Kann sterben (Methode)
+
+Kiste
+    - Kann verschlossen oder geöffnet sein (Eigenschaft) 
+    - Enthält Dinge (Eingenschaft) 
+    - Kann geöffnet oider geschlossen werden (Mehtode)
+---------------------------------------------------------------------------------------------------
+Klassen 
+
+Klassen sind Baupläne für Objekte und beschrieben diese 
+in einem Programm können beliebig viele Objekte von einer Klasse erzeugt werden 
+Beispielsweise kann man für die Klasse Auto unterschiedliche Eigenschaften festlegen wie z.B Hersteller, Baujahr, Farbe usw. 
+Dieser Klasse kann beliebig viele Objekte erzeugen, welchen jeweils völlig unterschiedliche Parameter für die Eigenschaften zugewiesen werden 
+z.B. Auto1: Rot Auto2: Blau Auto3: Schwarz 
+Alle Autos haben dieselben, vorher in der Klasse definierten Eigenschaften, allerdings wurden diesen Eigenschaften unterschiedliche Parameter zugeordnet
+Eine Klasse können wir also als eine Art "Rohbau" verstehen 
+*/
---
+++ JavacrashkursV2/src/i_objektorientierung/b_Klassen.java
@@ -0,0 +1,8 @@
+package i_objektorientierung;
+
+public class b_Klassen {
+    public static void main(String[] args) {
+        
+    }
+
+}
